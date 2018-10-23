bf6c8f939a21552cd7097fd6ff98a1800871ee3b
Martin Moene <martin.moene@gmail.com>
2018-10-23 22:13:25 +0200

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile
+
+class ValuePtrLiteConan(ConanFile):
+    version = "0.0.0"
+    name = "value-ptr-lite"
+    description = "A C++ smart-pointer with value semantics for C++98, C++11 and later"
+    license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
+    url = "https://github.com/martinmoene/value-ptr-lite"
+    exports_sources = "include/nonstd/*", "LICENSE.txt"
+    author = "Martin Moene"
+
+    def package_info(self):
+        self.info.header_only()
