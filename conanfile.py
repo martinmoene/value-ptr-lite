5a60d6f9e9dcec0cbc0650141da92cc1a0a5deb2
Martin Moene <martin.moene@gmail.com>
2018-10-24 00:22:53 +0200

    Correct url

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class ValuePtrLiteConan(ConanFile):
     name = "value-ptr-lite"
     description = "A C++ smart-pointer with value semantics for C++98, C++11 and later"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
-    url = "https://github.com/martinmoene/value-ptr-lite"
+    url = "https://github.com/martinmoene/value-ptr-lite.git"
     exports_sources = "include/nonstd/*", "LICENSE.txt"
     author = "Martin Moene"
 
